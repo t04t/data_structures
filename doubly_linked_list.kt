0ccf43737a465dffe0d2b0cc34f608afb311d03f
Toshiki Takabatake <gtr86nsx0801@gmail.com>
2024-01-11 22:19:13 +0900

    fix doubly linked list

--- doubly_linked_list.kt
+++ doubly_linked_list.kt
@@ -43,6 +43,54 @@ internal class DoublyLinkedList(arr: IntArray) {
         }
     }
 
+    fun preAppend(newNode: Node) {
+        head!!.prev = newNode
+        newNode.next = head
+        newNode.prev = null
+        head = newNode
+    }
+
+    fun tailAppend(newNode: Node) {
+        tail!!.next = newNode
+        newNode.prev = tail
+        newNode.next = null
+        tail = newNode
+    }
+
+    fun addNextAppend(node: Node, newNode: Node) {
+        var tempNode: Node? = node.next
+        node.next = newNode
+        newNode.next = tempNode
+        newNode.prev = node
+
+        if (node == tail) {
+            tail = newNode
+        } else {
+            tempNode!!.prev = newNode
+        }
+    }
+
+    fun popFront() {
+        head = head!!.next
+        head!!.prev = null
+    }
+
+    fun pop() {
+        tail = tail!!.prev
+        tail!!.next = null
+    }
+
+    fun deleteNode(node: Node) {
+        if (node == tail) {
+            pop()
+        } else if (node == head) {
+            popFront()
+        } else {
+            node.prev!!.next = node.next
+            node.next!!.prev = node.prev
+        }
+    }
+
     fun printList() {
         var iterator = head
         var str = ""
@@ -69,7 +117,20 @@ fun main() {
   val numList = DoublyLinkedList(numbers.toIntArray())
   numList.printList() // [1→2→3→4→5]
   println(numList.at(3)!!.data) // 4
+  numList.preAppend(Node(0))
+  numList.printList() // [0→1→2→3→4→5]
+  numList.tailAppend(Node(6))
+  numList.printList() // [0→1→2→3→4→5→6]
+  numList.addNextAppend(numList.at(3)!!, Node(7))
+  numList.printList() // [0→1→2→3→7→4→5→6]
+  numList.popFront()
+  numList.printList() // [1→2→3→7→4→5→6]
+  numList.pop()
+  numList.printList() // [1→2→3→7→4→5]
+  numList.deleteNode(numList.at(3)!!)
+  numList.printList() // [1→2→3→4→5]
   numList.reverse()
   numList.printList() // [5→4→3→2→1]
   numList.printReverse() // [1→2→3→4→5]
+
 }
